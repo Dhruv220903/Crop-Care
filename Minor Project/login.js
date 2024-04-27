76f475bded298814f2a176c582d628009b665a49
Dhruv Akte <89039156+Dhruv220903@users.noreply.github.com>
2024-04-27 14:20:47 +0530

    Added Register and login pages

---
+++ Minor Project/login.js
@@ -0,0 +1,36 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.11.1/firebase-app.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.11.1/firebase-analytics.js";
+import { getAuth, signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.11.1/firebase-auth.js";
+const firebaseConfig = {
+  apiKey: "AIzaSyBPTZsfnq_qHLxA__MwoCKaJ3o0sQ8BIGY",
+  authDomain: "mycc-eeecb.firebaseapp.com",
+  projectId: "mycc-eeecb",
+  storageBucket: "mycc-eeecb.appspot.com",
+  messagingSenderId: "35722063113",
+  appId: "1:35722063113:web:46930c3f11e6000e6cb7d8",
+  measurementId: "G-BZD9D57H34"
+};
+
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+
+const auth = getAuth(app);
+const button=document.querySelector("#create");
+button.addEventListener('click',function(event){
+  event.preventDefault();
+  const email=document.querySelector("#email").value;
+const password=document.querySelector("#password").value;
+
+  signInWithEmailAndPassword(auth, email, password)
+  .then((userCredential) => {
+    const user = userCredential.user;
+    window.location.href='demo.html';
+  })
+  .catch((error) => {
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    alert("invalid credentials");
+  });
+
+})
